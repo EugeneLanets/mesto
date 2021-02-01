26ed8e010704095ebdad4161660635458cd6f96e
Eugene Lanets <swarrt@yandex.ru>
2021-02-02 00:24:26 +0300

    feat: add initial cards data and card template

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,34 @@
+//initial cards
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+const cardTemplate = document.querySelector("#card-template").content;
+
+// Popup 
 const popup = document.querySelector(".popup");
 const editForm = popup.querySelector(".profile-form");
 const popupCloseButton = popup.querySelector(".popup__button");
