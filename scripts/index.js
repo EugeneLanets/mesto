e0a5011e21d6a612c9572a93b7885e4acc33c5d4
Eugene Lanets <swarrt@yandex.ru>
2021-02-05 19:34:16 +0300

    refactor: profile popup open and submit

--- scripts/index.js
+++ scripts/index.js
@@ -49,61 +49,65 @@ const formTemplate = document.querySelector("#modal-form");
 const popup = document.querySelector(".popup");
 
 const profileEditButton = document.querySelector(".profile__button_type_edit");
+const profileName = document.querySelector(".profile__name");
+const profileStatus = document.querySelector(".profile__status");
 const cardAddButton = document.querySelector(".profile__button_type_add");
-
-const profileFormData = {
-  name: "profile",
-  title: "Редактировать профиль",
-  inputs: [
-    {name: "name", placeholder: "Ваше имя"},
-    {name: "status", placeholder: "Пара слов о себе"}
-  ],
-  buttonName: "Сохранить",
-  submitCallback
-}
-
-const cardFormData = {
-  name: "add-card",
-  title: "Новое место",
-  inputs: [
-    {name: "name", placeholder: "Название"},
-    {name: "status", placeholder: "Ссылка на картинку"}
-  ],
-  buttonName: "Создать",
-  submitCallback
-}
-
 
 const createForm = formData => {
   const modalForm = formTemplate.content.cloneNode(true);
 
+  const form = modalForm.querySelector(".modal-form");
   const title = modalForm.querySelector(".modal-form__title");
   const inputs = modalForm.querySelectorAll("input[type=text]");
   const submitButton = modalForm.querySelector(".modal-form__submit");
   const closeButton = modalForm.querySelector(".modal-form__close");
 
   title.textContent = formData.title;
+
   inputs.forEach((item, idx) => {
     item.name = formData.inputs[idx].name;
-    item.placeholder = formData.inputs[idx].placeholder
+    item.placeholder = formData.inputs[idx].placeholder;
+    if (formData.name === "profile") {
+      item.value = [profileName, profileStatus][idx].textContent;
+    }
   });
+
   submitButton.textContent = formData.buttonName;
 
   closeButton.addEventListener("click", closePopup);
-  submitButton.addEventListener("submit", )
+  form.addEventListener("submit", handleProfileFormSubmit);
+  
 
   return modalForm;
 }
 
-const openPopup = formData => {
+const handleProfileFormSubmit = evt => {
+  evt.preventDefault();
+  const modalForm = evt.target;
+  const nameField = modalForm.querySelector("[name=name");
+  const statusField = modalForm.querySelector("[name=status");
+  profileName.textContent = nameField.value;
+  profileStatus.textContent = statusField.value;
+  closePopup();
+}
+
+const renderModalForm = formData => {
   popup.append(createForm(formData));
+}
+
+const removeModalForm = () => {
+  popup.querySelector(".modal-form").remove();
+}
+
+const openPopup = formData => {
+  renderModalForm(formData);
   popup.classList.add("popup_opened");
   document.addEventListener("keydown", handleEscPress);
 }
 
 const closePopup = () => {
   popup.classList.remove("popup_opened");
-  popup.querySelector(".modal-form").remove();
+  removeModalForm();
   document.removeEventListener("keydown", handleEscPress);
 }
 
@@ -120,4 +124,26 @@ profileEditButton.addEventListener("click", () => {
 cardAddButton.addEventListener("click", () => {
   openPopup(cardFormData);
 });
+
+const profileFormData = {
+  name: "profile",
+  title: "Редактировать профиль",
+  inputs: [
+    {name: "name", placeholder: "Ваше имя"},
+    {name: "status", placeholder: "Пара слов о себе"}
+  ],
+  buttonName: "Сохранить",
+  submitCallback: handleProfileFormSubmit
+}
+
+const cardFormData = {
+  name: "add-card",
+  title: "Новое место",
+  inputs: [
+    {name: "name", placeholder: "Название"},
+    {name: "link", placeholder: "Ссылка на картинку"}
+  ],
+  buttonName: "Создать",
+  submitCallback: undefined
+}
 
