76dadad493d7bbbbc4a788222b90ef56a879fe1e
Eugene Lanets <swarrt@yandex.ru>
2021-02-18 21:47:44 +0300

    feat: add validation & validation message for each input

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,5 +1,46 @@
+const showError = (formElement, inputElement, inputErrorClass, errorClass) => {
+  const errorMessageElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+  errorMessageElement.classList.add(errorClass);
+  errorMessageElement.textContent = inputElement.validationMessage;
+
+  inputElement.classList.add(inputErrorClass);
+}
+
+const hideError = (formElement, inputElement, inputErrorClass, errorClass) => {
+  const errorMessageElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+  errorMessageElement.classList.remove(errorClass);
+  errorMessageElement.textContent = "";
+
+  inputElement.classList.remove(inputErrorClass);
+}
+
+
+
+const checkInputValidity = (formElement, inputElement, inputErrorClass, errorClass) => {
+  if (!inputElement.validity.valid) {
+    showError(formElement, inputElement, inputErrorClass, errorClass);
+  } else {
+    hideError(formElement, inputElement, inputErrorClass, errorClass);
+  }
+}
+
+const setEventListeners = (formElement, inputSelector, inputErrorClass, errorClass) => {
+  const inputsList = formElement.querySelectorAll(inputSelector);
+  inputsList.forEach(inputElement => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement, inputErrorClass, errorClass)});
+  });
+}
+
 const enableValidation = ({formSelector, inputSelector, submitButtonsSelector, inactiveButtonClass, inputErrorClass, errorClass}) => {
-  console.log("hi");
+  console.log("Forms validation enabled");
+  const formsList = document.querySelectorAll(formSelector);
+  
+  formsList.forEach(formElement => {
+    setEventListeners(formElement, inputSelector, inputErrorClass, errorClass);
+  })
 }
 
 enableValidation({
