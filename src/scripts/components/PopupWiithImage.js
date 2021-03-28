5085e63cad9e180133c62e416f547fc600c04ffa
Eugene Lanets <swarrt@yandex.ru>
2021-03-28 20:53:45 +0300

    feat: add class PopupWithImage

---
+++ src/scripts/components/PopupWiithImage.js
@@ -0,0 +1,17 @@
+import Popup from "./Popup";
+
+
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._image = this._popup.querySelector(".big-picture__image");
+    this._caption = this._popup.querySelector(".big-picture__caption");
+  }
+
+  open(link, caption) {
+    this._image.src = link;
+    this._image.alt = caption;
+    this._caption.textContent = caption;
+    super.open()
+  }
+}
